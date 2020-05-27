b643dc8d124ef628afebd19176186d6e7df8ab60
desittisagar <dsagar.smart@gmail.com>
2020-05-27 14:41:48 +0530

    final product

--- articles/views.py
+++ articles/views.py
@@ -3,33 +3,54 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from .models import Article
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 # Create your views here.
 
 #ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
 	model = Article
 	template_name = 'article_new.html'
 	fields = ('title', 'body')
+	login_url = 'login'
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
 	template_name = 'article_list.html'
 	model = Article
+	login_url = 'login'
 
-class ArticleDetailView(DetailView):
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
 	template_name = 'article_detail.html'
 	model = Article
+	login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
 	template_name = 'article_edit.html'
 	model = Article
 	fields = ('title', 'body')
+	login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+	def dispatch(self, request, *args, **kwargs):
+		obj = self.get_object()
+		if obj.author != self.request.user:
+			raise PermissionDenied
+		return super().dispatch(request, *args, **kwargs)
+
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
 	template_name = 'article_delete.html'
 	model = Article
 	success_url = reverse_lazy('article_list')
+	login_url = 'login'
+
+	def dispatch(self, request, *args, **kwargs):
+		obj = self.get_object()
+		if obj.author != self.request.user:
+			raise PermissionDenied
+		return super().dispatch(request, *args, **kwargs)
